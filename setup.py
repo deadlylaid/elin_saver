64efbe4b00cce39dd29b59a7af34caa6ce9002e2
deadlylaid <deadlylaid@gmail.com>
2018-12-16 23:15:34 +0900

    setup initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup_requires = [
+        'setuptools>=35.0.2',
+]
+
+
+setup(
+        name='elin_saver',
+        version='0.1',
+        url='https://github.com/deadlylaid/elin_saver',
+        author_email='deadlylaid@gmail.com'
+        )
